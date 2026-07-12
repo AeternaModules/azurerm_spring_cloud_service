3d3fb60a1ba8c657d4b54dc6c0c2de0fa90d5e52
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,88 +1,83 @@
-output "spring_cloud_services" {
-  description = "All spring_cloud_service resources"
-  value       = azurerm_spring_cloud_service.spring_cloud_services
-  sensitive   = true
-}
 output "spring_cloud_services_build_agent_pool_size" {
-  description = "List of build_agent_pool_size values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.build_agent_pool_size]
+  description = "Map of build_agent_pool_size values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.build_agent_pool_size }
 }
 output "spring_cloud_services_config_server_git_setting" {
-  description = "List of config_server_git_setting values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.config_server_git_setting]
+  description = "Map of config_server_git_setting values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.config_server_git_setting }
   sensitive   = true
 }
 output "spring_cloud_services_container_registry" {
-  description = "List of container_registry values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.container_registry]
+  description = "Map of container_registry values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.container_registry }
   sensitive   = true
 }
 output "spring_cloud_services_default_build_service" {
-  description = "List of default_build_service values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.default_build_service]
+  description = "Map of default_build_service values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.default_build_service }
 }
 output "spring_cloud_services_location" {
-  description = "List of location values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.location]
+  description = "Map of location values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.location }
 }
 output "spring_cloud_services_log_stream_public_endpoint_enabled" {
-  description = "List of log_stream_public_endpoint_enabled values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.log_stream_public_endpoint_enabled]
+  description = "Map of log_stream_public_endpoint_enabled values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.log_stream_public_endpoint_enabled }
 }
 output "spring_cloud_services_managed_environment_id" {
-  description = "List of managed_environment_id values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.managed_environment_id]
+  description = "Map of managed_environment_id values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.managed_environment_id }
 }
 output "spring_cloud_services_marketplace" {
-  description = "List of marketplace values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.marketplace]
+  description = "Map of marketplace values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.marketplace }
 }
 output "spring_cloud_services_name" {
-  description = "List of name values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.name]
+  description = "Map of name values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.name }
 }
 output "spring_cloud_services_network" {
-  description = "List of network values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.network]
+  description = "Map of network values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.network }
 }
 output "spring_cloud_services_outbound_public_ip_addresses" {
-  description = "List of outbound_public_ip_addresses values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.outbound_public_ip_addresses]
+  description = "Map of outbound_public_ip_addresses values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.outbound_public_ip_addresses }
 }
 output "spring_cloud_services_required_network_traffic_rules" {
-  description = "List of required_network_traffic_rules values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.required_network_traffic_rules]
+  description = "Map of required_network_traffic_rules values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.required_network_traffic_rules }
 }
 output "spring_cloud_services_resource_group_name" {
-  description = "List of resource_group_name values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.resource_group_name]
+  description = "Map of resource_group_name values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.resource_group_name }
 }
 output "spring_cloud_services_service_registry_enabled" {
-  description = "List of service_registry_enabled values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.service_registry_enabled]
+  description = "Map of service_registry_enabled values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.service_registry_enabled }
 }
 output "spring_cloud_services_service_registry_id" {
-  description = "List of service_registry_id values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.service_registry_id]
+  description = "Map of service_registry_id values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.service_registry_id }
 }
 output "spring_cloud_services_sku_name" {
-  description = "List of sku_name values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.sku_name]
+  description = "Map of sku_name values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.sku_name }
 }
 output "spring_cloud_services_sku_tier" {
-  description = "List of sku_tier values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.sku_tier]
+  description = "Map of sku_tier values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.sku_tier }
 }
 output "spring_cloud_services_tags" {
-  description = "List of tags values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.tags]
+  description = "Map of tags values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.tags }
 }
 output "spring_cloud_services_trace" {
-  description = "List of trace values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.trace]
+  description = "Map of trace values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.trace }
 }
 output "spring_cloud_services_zone_redundant" {
-  description = "List of zone_redundant values across all spring_cloud_services"
-  value       = [for k, v in azurerm_spring_cloud_service.spring_cloud_services : v.zone_redundant]
+  description = "Map of zone_redundant values across all spring_cloud_services, keyed the same as var.spring_cloud_services"
+  value       = { for k, v in azurerm_spring_cloud_service.spring_cloud_services : k => v.zone_redundant }
 }
 
